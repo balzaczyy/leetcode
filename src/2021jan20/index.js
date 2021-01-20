bcf4c57e3bc1bd8c506e92ed403aa82d9e58ea52
zhouyiyan <balzaczyy@gmail.com>
2021-01-20 22:39:51 +0800

    2021jan20

---
+++ src/2021jan20/index.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = function(s) {
+  const stack = [];
+
+  for (const ch of s) {
+    switch (ch) {
+      case '(':
+      case '{':
+      case '[':
+        stack.push(ch);
+        break;
+      case ')':
+        if (stack.pop() !== '(') {
+          return false;
+        }
+        break;
+      case '}':
+        if (stack.pop() !== '{') {
+          return false;
+        }
+        break;
+      case ']':
+        if (stack.pop() !== '[') {
+          return false;
+        }
+        break;
+    }
+  }
+  return stack.length === 0;
+};
+
+export default function run(input) {
+  return input.map(isValid).map(v => String(v));
+}
