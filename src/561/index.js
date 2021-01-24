76d5e42866761f326f229ce310099b2c0cbdc9b4
zhouyiyan <balzaczyy@gmail.com>
2021-01-24 19:32:57 +0800

    561 test

---
+++ src/561/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const arrayPairSum = function (nums) {
+  return 0;
+};
+
+export default function run(input) {
+  return input.map(JSON.parse).map(arrayPairSum).map(String);
+}
