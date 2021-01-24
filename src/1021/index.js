17c57ee708d7186890d10a1f6c6993ba33999ed2
zhouyiyan <balzaczyy@gmail.com>
2021-01-24 16:11:14 +0800

    1021

--- src/1021/index.js
+++ src/1021/index.js
@@ -3,7 +3,22 @@
  * @return {string}
  */
 const removeOuterParentheses = function (S) {
-  return S;
+  const parts = [];
+  let depth = 0,
+    from = 0;
+  for (let i = 0; i < S.length; i++) {
+    if (S[i] === "(") {
+      depth++;
+    } else {
+      depth--;
+      if (depth === 0) {
+        parts.push(S.substring(from, i + 1));
+        from = i + 1;
+      }
+    }
+  }
+  const ans = parts.map((v) => v.substring(1, v.length - 1));
+  return ans.join("");
 };
 
 export default function run(input) {
