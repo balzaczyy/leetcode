4015fb6fa75b7e611fb8218a5d7a585ca3048dae
zhouyiyan <balzaczyy@gmail.com>
2021-01-25 16:02:25 +0800

    1365

--- src/1365/index.js
+++ src/1365/index.js
@@ -3,7 +3,22 @@
  * @return {number[]}
  */
 const smallerNumbersThanCurrent = function (nums) {
-  return nums;
+  const s = nums.slice();
+  s.sort((a, b) => a - b);
+  const table = Array(10);
+  table[s[0]] = 0;
+  let count = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (s[i] > s[i - 1]) {
+      table[s[i]] = table[s[i - 1]] + count;
+      count = 1;
+    } else {
+      table[s[i]] = table[s[i - 1]];
+      count++;
+    }
+  }
+  // console.log(table);
+  return nums.map((v) => table[v]);
 };
 
 export default function run(input) {
