70e7fa884657019ef50dcde319d3b3656692df1d
zhouyiyan <balzaczyy@gmail.com>
2021-01-25 14:20:59 +0800

    1187 fail

--- src/1187/index.js
+++ src/1187/index.js
@@ -7,6 +7,47 @@ import { group } from "../utils.js";
  */
 
 const makeArrayIncreasing = function (arr1, arr2) {
+  // filter arr2 and remove elements that exist in arr1
+  const values = arr1.reduce((acc, cur) => {
+    acc.add(cur);
+    return acc;
+  }, new Set());
+  const arr2f = arr2.filter((v) => !values.has(v));
+  arr2f.sort((a, b) => a - b);
+
+  // DP to calculate LIS
+  const nextInArr2 = Array(arr1.length).fill(0);
+  const ans = Array(arr1.length).fill(0);
+  for (let i = 0; i < arr1.length - 1; i++) {
+    for (let j = i + 1; j < arr1.length; j++) {
+      if (arr1[j] > arr1[i]) {
+        continue; // ok
+      }
+      let k = nextInArr2[i];
+      if (i > 0) {
+        while (k < arr2f.length && arr2f[k] <= arr1[i - 1]) {
+          k++;
+        }
+        if (k === arr2f.length) {
+          return -1;
+        }
+      }
+      if (arr2f[k] < arr1[j]) {
+        // found one match
+        nextInArr2[j] = k;
+        ans[j] = ans[i] + 1;
+      } else {
+        // have to change myself
+        while (k < arr2f.length && arr2f[k] <= arr1[i]) {
+          k++;
+        }
+        if (k === arr2f.length) {
+          return -1;
+        }
+      }
+    }
+  }
+
   return -1;
 };
 
