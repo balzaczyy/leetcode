a6c24a627fafb34f478f6548c2adf8f985330a3d
zhouyiyan <balzaczyy@gmail.com>
2021-01-25 16:45:08 +0800

    1475

--- src/1475/index.js
+++ src/1475/index.js
@@ -3,7 +3,16 @@
  * @return {number[]}
  */
 const finalPrices = function (prices) {
-  return prices;
+  return prices.map((v, i) => {
+    let j = i + 1;
+    while (j < prices.length && prices[j] > prices[i]) {
+      j++;
+    }
+    if (j === prices.length) {
+      return v;
+    }
+    return v - prices[j];
+  });
 };
 
 export default function run(input) {
