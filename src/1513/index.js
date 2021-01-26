651370065fbfa1fa760ed9b68813db97526f92f0
zhouyiyan <balzaczyy@gmail.com>
2021-01-26 10:57:52 +0800

    1513

--- src/1513/index.js
+++ src/1513/index.js
@@ -3,7 +3,30 @@
  * @return {number}
  */
 const numSub = function (s) {
-  return 0;
+  const mod = 1000000007;
+  function count(n) {
+    let ans = 0;
+    for (let i = 1; i <= n; i++) {
+      ans = (ans + i) % mod;
+    }
+    return ans;
+  }
+
+  let from = 0,
+    ans = 0;
+  do {
+    from = s.indexOf("1", from);
+    if (from < 0) {
+      break;
+    }
+    let to = s.indexOf("0", from + 1);
+    if (to < 0) {
+      to = s.length;
+    }
+    ans = (ans + count(to - from)) % mod;
+    from = to;
+  } while (from < s.length);
+  return ans;
 };
 
 export default function run(input) {
