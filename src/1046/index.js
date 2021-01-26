456ddbdd52514b199a2051ea65cba82669d30f4b
zhouyiyan <balzaczyy@gmail.com>
2021-01-26 11:06:28 +0800

    1046

--- src/1046/index.js
+++ src/1046/index.js
@@ -3,7 +3,18 @@
  * @return {number}
  */
 const lastStoneWeight = function (stones) {
-  return 0;
+  while (stones.length > 1) {
+    // should use heap sort to obtain best performance
+    // use qsort for easy implementation
+    stones.sort((a, b) => b - a);
+    const d = stones[0] - stones[1];
+    if (d === 0) {
+      stones.splice(0, 2);
+    } else {
+      stones.splice(0, 2, d);
+    }
+  }
+  return stones[0] || 0;
 };
 
 export default function run(input) {
