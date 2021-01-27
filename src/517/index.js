ee6fe43d52dd7556905d40fd9431988e60b5dae1
zhouyiyan <balzaczyy@gmail.com>
2021-01-27 13:33:58 +0800

    517

--- src/517/index.js
+++ src/517/index.js
@@ -3,7 +3,22 @@
  * @return {number}
  */
 const findMinMoves = function (machines) {
-  return 0;
+  const sum = machines.reduce((acc, cur) => acc + cur);
+  if (sum % machines.length !== 0) {
+    return -1;
+  }
+
+  const avg = sum / machines.length;
+  let s = 0,
+    ans = 0;
+  for (let i = 0; i < machines.length; i++) {
+    s += machines[i] - avg;
+    ans = Math.max(ans, Math.abs(s));
+    if (machines[i] > avg) {
+      ans = Math.max(ans, Math.abs(machines[i] - avg));
+    }
+  }
+  return ans;
 };
 
 export default function run(input) {
