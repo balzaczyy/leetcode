59fe74ed90f057ffc093a1ab2f8b7720380bc426
zhouyiyan <balzaczyy@gmail.com>
2021-01-27 08:49:42 +0800

    2021jan26

--- src/2021/Jan/26/index.js
+++ src/2021/Jan/26/index.js
@@ -3,7 +3,40 @@
  * @return {number}
  */
 const minimumEffortPath = function (heights) {
-  return 0;
+  const rows = heights.length;
+  const cols = heights[0].length;
+  const visited = Array(rows * cols);
+  const id = (row, col) => row * cols + col;
+  visited[0] = { row: 0, col: 0, effort: 0 };
+  const q = [visited[0]];
+  const tryMove = (row, col, h, effort) => {
+    const s = visited[id(row, col)];
+    if (!s) {
+      const e = Math.max(effort, Math.abs(heights[row][col] - h));
+      const s = { row, col, effort: e };
+      visited[id(row, col)] = s;
+      q.push(s);
+    } else {
+      const e = Math.max(effort, Math.abs(heights[row][col] - h));
+      s.effort = Math.min(s.effort, e);
+    }
+  };
+
+  while (q.length > 0) {
+    const { row, col, effort } = q.shift();
+    // console.log(row, col, effort);
+    if (row === rows - 1 && col === cols - 1) {
+      return effort;
+    }
+    const h = heights[row][col];
+    if (row > 0) tryMove(row - 1, col, h, effort);
+    if (row < rows - 1) tryMove(row + 1, col, h, effort);
+    if (col > 0) tryMove(row, col - 1, h, effort);
+    if (col < cols - 1) tryMove(row, col + 1, h, effort);
+    q.sort((a, b) => a.effort - b.effort);
+    // console.log(q);
+  }
+  throw new Error("impossible");
 };
 
 export default function run(input) {
