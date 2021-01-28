f4058cbaab1b51b3807125481e64498f573d0b57
zhouyiyan <balzaczyy@gmail.com>
2021-01-28 17:57:06 +0800

    2021/jan/28

--- src/2021/Jan/28/index.js
+++ src/2021/Jan/28/index.js
@@ -6,9 +6,23 @@ import { group } from "../../../utils.js";
  * @return {string}
  */
 const getSmallestString = function (n, k) {
-  return "";
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const ans = Array(n).fill("a");
+  k -= n;
+  for (let i = n - 1; i >= 0; i--) {
+    if (k >= 25) {
+      ans[i] = "z";
+      k -= 25;
+    } else {
+      ans[i] = alphabet[k];
+      break;
+    }
+  }
+  return ans.join("");
 };
 
 export default function run(input) {
-  return group(input.map(JSON.parse), 2).map((n, k) => getSmallestString(n, k));
+  return group(input.map(JSON.parse), 2).map(([n, k]) =>
+    getSmallestString(n, k)
+  );
 }
