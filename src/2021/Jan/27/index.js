518273bebf27f0a7b2d465428f3559049102d1c5
zhouyiyan <balzaczyy@gmail.com>
2021-01-28 08:17:03 +0800

    2021jan27

--- src/2021/Jan/27/index.js
+++ src/2021/Jan/27/index.js
@@ -3,7 +3,14 @@
  * @return {number}
  */
 const concatenatedBinary = function (n) {
-  return n;
+  const mod = 1000000007;
+  const moveLeft = (n) => Math.pow(2, Math.floor(Math.log2(n)) + 1);
+  let d = 1;
+  for (let i = 2; i <= n; i++) {
+    d = (d * moveLeft(i) + i) % mod;
+    // console.log(i, d);
+  }
+  return d;
 };
 
 export default function run(input) {
