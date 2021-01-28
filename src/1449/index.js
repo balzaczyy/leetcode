fe618a7b242856c68ff057b4f552c64e95a81bff
zhouyiyan <balzaczyy@gmail.com>
2021-01-28 15:52:04 +0800

    1449 not greedy search

--- src/1449/index.js
+++ src/1449/index.js
@@ -6,7 +6,59 @@ import { group } from "../utils.js";
  * @return {string}
  */
 const largestNumber = function (cost, target) {
-  return "7772";
+  const cost2 = (() => {
+    const cd = cost.map((cost, i) => ({ cost, number: i + 1 }));
+    cd.sort((a, b) => {
+      if (a.cost === b.cost) {
+        return b.number - a.number;
+      }
+      return a.cost - b.cost;
+    });
+    return cd.reduce((acc, cur) => {
+      if (acc.length === 0 || acc[acc.length - 1].cost !== cur.cost) {
+        acc.push(cur);
+      }
+      return acc;
+    }, []);
+  })();
+  console.log(cost2);
+
+  const combo = [];
+  function search(pos = 0, total = target) {
+    if (total === 0) {
+      // console.log(combo);
+      return true;
+    } else if (pos === cost2.length) {
+      return false;
+    }
+
+    const cur = cost2[pos];
+    const limit = Math.floor(total / cur.cost);
+    for (let i = limit; i > 0; i--) {
+      combo.push({ number: cur.number, times: i });
+      if (search(pos + 1, total - cur.cost * i)) {
+        return true;
+      }
+      combo.pop();
+    }
+    return search(pos + 1, total);
+  }
+
+  const found = search();
+  if (!found) {
+    return "0";
+  }
+
+  combo.sort((a, b) => b.number - a.number);
+  // console.log(combo);
+  return combo
+    .reduce((acc, cur) => {
+      for (let i = 0; i < cur.times; i++) {
+        acc.push(String(cur.number));
+      }
+      return acc;
+    }, [])
+    .join("");
 };
 
 export default function run(input) {
