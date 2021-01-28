3036fcd6836debd51ec78ae5ee35221c1f21dd1c
zhouyiyan <balzaczyy@gmail.com>
2021-01-28 17:32:31 +0800

    1449

--- src/1449/index.js
+++ src/1449/index.js
@@ -21,44 +21,62 @@ const largestNumber = function (cost, target) {
       return acc;
     }, []);
   })();
-  console.log(cost2);
+  // console.log(cost2);
 
-  const combo = [];
-  function search(pos = 0, total = target) {
-    if (total === 0) {
-      // console.log(combo);
-      return true;
-    } else if (pos === cost2.length) {
-      return false;
+  function compare(a, b) {
+    if (a[0] !== b[0]) {
+      throw new Error("impossible");
     }
-
-    const cur = cost2[pos];
-    const limit = Math.floor(total / cur.cost);
-    for (let i = limit; i > 0; i--) {
-      combo.push({ number: cur.number, times: i });
-      if (search(pos + 1, total - cur.cost * i)) {
-        return true;
+    for (let i = 9; i >= 1; i--) {
+      const d = (a[i] || 0) - (b[i] || 0);
+      if (d !== 0) {
+        return d;
       }
-      combo.pop();
     }
-    return search(pos + 1, total);
+    return 0;
   }
 
-  const found = search();
-  if (!found) {
+  const table = [[0]];
+  for (let i = 1; i <= target; i++) {
+    let combo = [0];
+    for (let j = 0; j < cost2.length && cost2[j].cost <= i; j++) {
+      const { cost, number } = cost2[j];
+      if (!table[i - cost]) continue;
+
+      const len2 = table[i - cost][0] + 1;
+      if (len2 > combo[0]) {
+        const arr = table[i - cost].slice();
+        arr[0]++;
+        arr[number] = (arr[number] || 0) + 1;
+        combo = arr;
+      } else if (len2 < combo[0]) {
+        // skip
+      } else {
+        const arr = table[i - cost].slice();
+        arr[0]++;
+        arr[number] = (arr[number] || 0) + 1;
+        if (compare(arr, combo) > 0) {
+          combo = arr;
+        }
+      }
+    }
+    if (combo[0] > 0) {
+      table[i] = combo;
+    }
+  }
+  // console.log(table);
+  const ans = table[target];
+  if (!ans) {
     return "0";
   }
-
-  combo.sort((a, b) => b.number - a.number);
-  // console.log(combo);
-  return combo
-    .reduce((acc, cur) => {
-      for (let i = 0; i < cur.times; i++) {
-        acc.push(String(cur.number));
-      }
-      return acc;
-    }, [])
-    .join("");
+  // console.log(ans);
+  const s = [];
+  for (let i = ans.length - 1; i > 0; i--) {
+    for (let j = 0; j < ans[i]; j++) {
+      s.push(String(i));
+    }
+  }
+  return s.join("");
 };
 
 export default function run(input) {
