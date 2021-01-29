f787d5ebaeca481a4918dcdacb8c41e66c45a3a3
zhouyiyan <balzaczyy@gmail.com>
2021-01-29 19:30:06 +0800

    2021jan29

--- src/2021/Jan/29/index.js
+++ src/2021/Jan/29/index.js
@@ -13,7 +13,47 @@ import { arrayToTree } from "../../../897/index.js";
  * @return {number[][]}
  */
 const verticalTraversal = function (root) {
-  return [];
+  const nodesByX = new Map();
+  let minX = 0,
+    maxX = 0;
+  function record(point) {
+    const x = point.x;
+    minX = Math.min(minX, x);
+    maxX = Math.max(maxX, x);
+    let nodes = nodesByX.get(x);
+    if (!nodes) {
+      nodes = [];
+      nodesByX.set(x, nodes);
+    }
+    nodes.push(point);
+  }
+
+  const q = [{ node: root, x: 0, y: 0 }];
+  while (q.length > 0) {
+    const { node, x, y } = q.shift();
+
+    record({ value: node.val, x, y });
+    if (node.left) {
+      q.push({ node: node.left, x: x - 1, y: y - 1 });
+    }
+    if (node.right) {
+      q.push({ node: node.right, x: x + 1, y: y - 1 });
+    }
+  }
+
+  const ans = [];
+  for (let i = minX; i <= maxX; i++) {
+    const nodes = nodesByX.get(i);
+    nodes.sort((a, b) => {
+      const dy = b.y - a.y;
+      if (dy !== 0) {
+        return dy;
+      }
+      return a.value - b.value;
+    });
+    ans.push(nodes.map((v) => v.value));
+  }
+  return ans;
 };
 
 export default function run(input) {
