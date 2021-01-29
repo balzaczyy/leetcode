0da3ac07a81aaff18208be14e9945772c1d85efb
zhouyiyan <balzaczyy@gmail.com>
2021-01-29 11:39:48 +0800

    9

--- src/9/index.js
+++ src/9/index.js
@@ -3,7 +3,15 @@
  * @return {boolean}
  */
 const isPalindrome = function (x) {
-  return x >= 0;
+  if (x < 0) {
+    return false;
+  }
+
+  let d = 0;
+  for (let i = x; i > 0; i = Math.floor(i / 10)) {
+    d = d * 10 + (i % 10);
+  }
+  return d === x;
 };
 
 export default function run(input) {
