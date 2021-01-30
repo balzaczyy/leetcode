ddf6756e47f6ccdf7ed96b9a3e3817904987c407
zhouyiyan <balzaczyy@gmail.com>
2021-01-30 09:10:46 +0800

    1049

--- src/1049/index.js
+++ src/1049/index.js
@@ -3,7 +3,18 @@
  * @return {number}
  */
 const lastStoneWeightII = function (stones) {
-  return 0;
+  const sum = stones.reduce((acc, cur) => acc + cur);
+  const target = Math.floor(sum / 2);
+  const f = Array(target + 1);
+  for (let i = 0; i < stones.length; i++) {
+    for (let l = target; l >= stones[i]; l--) {
+      if ((f[l - stones[i]] || 0) + stones[i] > (f[l] || 0)) {
+        f[l] = (f[l - stones[i]] || 0) + stones[i];
+      }
+    }
+  }
+  // console.log(target, f);
+  return Math.abs(sum - f[target] * 2);
 };
 
 export default function run(input) {
