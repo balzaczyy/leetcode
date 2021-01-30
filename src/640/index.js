83878e47a0d5c3c160b95bb26c5fa06c6ee0708a
zhouyiyan <balzaczyy@gmail.com>
2021-01-30 12:59:39 +0800

    640

--- src/640/index.js
+++ src/640/index.js
@@ -3,7 +3,41 @@
  * @return {string}
  */
 const solveEquation = function (equation) {
-  return "No solution";
+  let numX = 0,
+    value = 0;
+  equation += "$";
+  let stack = "",
+    eqOffset = 1;
+  for (const ch of equation) {
+    if ("+-=$".includes(ch)) {
+      // stop sign
+      const sign = stack.startsWith("-") ? -1 : 1;
+      if (sign < 0) {
+        stack = stack.substring(1);
+      }
+      if (stack.startsWith("+")) {
+        stack = stack.substring(1);
+      }
+      if (stack.endsWith("x")) {
+        numX +=
+          eqOffset * sign * Number(stack.substring(0, stack.length - 1) || "1");
+      } else {
+        value -= eqOffset * sign * Number(stack);
+      }
+      if (ch === "=") {
+        eqOffset = -1;
+        stack = "";
+      } else {
+        stack = ch;
+      }
+    } else {
+      stack += ch;
+    }
+  }
+  if (numX === 0) {
+    return value === 0 ? "Infinite solutions" : "No solution";
+  }
+  return "x=" + String(value / numX);
 };
 
 export default function run(input) {
