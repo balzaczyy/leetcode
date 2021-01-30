c2fdf77657f7b84a2b54ceb80896d87ce361956d
zhouyiyan <balzaczyy@gmail.com>
2021-01-30 14:10:36 +0800

    466 slow

--- src/466/index.js
+++ src/466/index.js
@@ -8,7 +8,17 @@ import { group } from "../utils.js";
  * @return {number}
  */
 const getMaxRepetitions = function (s1, n1, s2, n2) {
-  return 0;
+  const len1 = s1.length * n1;
+  let j = 0;
+  for (let i = 0; i < len1; i++) {
+    // const g1 = Math.floor(i/s1.length);
+    const p1 = i % s1.length;
+    const p2 = j % s2.length;
+    if (s2[p2] === s1[p1]) {
+      j++;
+    }
+  }
+  return Math.floor(j / s2.length / n2);
 };
 
 export default function run(input) {
