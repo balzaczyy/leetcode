afe90e62b072d6062602f066100a61da1efe00d4
zhouyiyan <balzaczyy@gmail.com>
2021-01-30 20:48:48 +0800

    2021/jan/30 TLE

--- src/2021/Jan/30/index.js
+++ src/2021/Jan/30/index.js
@@ -2,8 +2,30 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minimumDeviation = function (nums) {
-  return 0;
+const minimumDeviation = function (nums) {
+  const findMin = (v) => {
+    while (v % 2 === 0) v /= 2;
+    return v;
+  };
+  const findMax = (v) => (v % 2 === 0 ? v : v * 2);
+  const state = nums.map((v) => ({
+    value: findMin(v),
+    max: findMax(v),
+  }));
+  let minDev = -1;
+  while (true) {
+    state.sort((a, b) => a.value - b.value);
+    let maxDev = state[state.length - 1].value - state[0].value;
+    if (minDev === -1 || maxDev < minDev) {
+      minDev = maxDev;
+    }
+    if (state[0].value < state[0].max) {
+      state[0].value *= 2;
+    } else {
+      break;
+    }
+  }
+  return minDev;
 };
 
 export default function run(input) {
