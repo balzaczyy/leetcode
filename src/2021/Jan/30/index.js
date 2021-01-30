e8c82fb54b98143d038ba3f17f43acb848309e72
zhouyiyan <balzaczyy@gmail.com>
2021-01-30 17:51:49 +0800

    2021/jan/30 test

---
+++ src/2021/Jan/30/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDeviation = function (nums) {
+  return 0;
+};
+
+export default function run(input) {
+  return input.map(JSON.parse).map(minimumDeviation).map(String);
+}
