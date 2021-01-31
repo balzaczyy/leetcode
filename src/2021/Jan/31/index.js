c4651d1299d8ec50012e27f80ffe8c90970afc45
zhouyiyan <balzaczyy@gmail.com>
2021-01-31 20:41:28 +0800

    2021/jan/31

--- src/2021/Jan/31/index.js
+++ src/2021/Jan/31/index.js
@@ -2,7 +2,36 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const nextPermutation = function (nums) {};
+const nextPermutation = function (nums) {
+  let found = -1;
+  for (let i = nums.length - 2; i >= 0 && found < 0; i--) {
+    let next = -1;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] > nums[i] && (next < 0 || nums[j] < nums[next])) {
+        next = j;
+      }
+    }
+    if (next >= 0) {
+      const t = nums[i];
+      nums[i] = nums[next];
+      nums[next] = t;
+      found = i;
+    }
+  }
+
+  if (found >= 0) {
+    if (found < nums.length - 1) {
+      nums
+        .slice(found + 1)
+        .sort((a, b) => a - b)
+        .forEach((v, i) => {
+          nums[found + 1 + i] = v;
+        });
+    }
+  } else {
+    nums.sort((a, b) => a - b);
+  }
+};
 
 export default function run(input) {
   return input
