0085a2684d76d458b4acc5efcf53a409188e338c
zhouyiyan <balzaczyy@gmail.com>
2021-02-01 18:02:35 +0800

    564 save

--- src/564/index.js
+++ src/564/index.js
@@ -3,67 +3,80 @@
  * @return {string}
  */
 const nearestPalindromic = function (n) {
-  const len = Math.ceil(n.length / 2);
-  let a = Number(n.substring(0, len));
-  let b = Number(n.substring(len));
+  if (n.length <= 2) {
+    const d = Number(n);
+    if (d <= 11) {
+      return String(d - 1);
+    }
+  }
+
+  const len = Math.floor(n.length / 2);
+  const a = Number(n.substring(0, len));
+  const b = Number(n.substring(n.length - len));
+  const seed = Number(n.substring(0, Math.ceil(n.length / 2)));
   const hasMid = n.length % 2 === 1;
-  console.log(n, a, b);
 
   const reverse = (n) => {
     let r = 0,
-      t = n,
-      i = 0;
+      t = n;
     while (t > 0) {
       r = r * 10 + (t % 10);
       t = Math.floor(t / 10);
-      i++;
-    }
-    if (len + i > n.length) {
-      // exclude mid
-      return reverse(Math.floor(n / 10));
     }
     return r;
   };
 
-  // const padding = (s, size) => {
-  //   if (s.length > size + 1) {
-  //     throw new Error('impossible');
-  //   } else if (s.length === size + 1) {
-  //     return s.substring(1);
-  //   } else if (s.length === size) {
-  //     return s;
-  //   }
-  //   return Array(size - s.length).fill('0').join('') + s;
-  // }
-
-  const floor = Math.pow(10, len);
   const r = reverse(a);
-  if (r < b) {
-    // try add 1
-    const d = Math.abs(r - b);
-    const r1 = reverse(a + 1);
-    const d1 = Math.abs(r1 + floor - b);
-    if (d1 < d) {
-      a++;
-    }
-  } else if (r > b) {
-    const d = Math.abs(r - b);
-    const r1 = reverse(a - 1);
-    const d1 = Math.abs(b + floor - r1);
-    if (d1 < d) {
-      a--;
-    }
-  } else {
-    return "oh no";
+  const ans = [];
+  if (r !== b) {
+    ans.push({
+      value: String(seed) + String(r),
+      diff: Math.abs(r - b),
+    });
   }
+  if (r >= b) {
+    const numDigits = String(seed - 1).length;
+    // if (numDigits < len) {
+    //   return 'oh no';
+    // }
+    //TODO 偶数位坍缩 设置mid=9
+    const floor = Math.pow(10, len);
+    const r1 = reverse(
+      hasMid && numDigits > len ? Math.floor((seed - 1) / 10) : seed - 1
+    );
+    ans.push({
+      value: String(seed - 1) + String(r1),
+      diff: b + floor - r1,
+    });
+  }
+  if (r <= b) {
+    const numDigits = String(seed + 1).length;
+    if (numDigits > len) {
+      return "oh no";
+    }
+    const floor = Math.pow(10, len);
+    const r1 = reverse(hasMid ? Math.floor((seed + 1) / 10) : seed + 1);
+    ans.push({
+      value: String(seed + 1) + String(r1),
+      diff: r1 + floor - b,
+    });
+  }
+  ans.sort((a, b) => {
+    const d = a.diff - b.diff;
+    if (d !== 0) {
+      return d;
+    }
+    return a.value.localeCompare(b.value);
+  });
+  return ans[0].value;
 
-  const pa = String(a);
-  const pb = String(reverse(a));
-  const padding = n.length - len - pb.length;
-  if (padding < 0) {
-    throw new Error("impossible");
-  }
-  return padding === 0 ? pa + pb : pa + Array().fill("0").join("") + pb;
+  // const pa = String(a);
+  // const pb = String(reverse(a));
+  // const padding = n.length - len - pb.length;
+  // if (padding < 0) {
+  //   throw new Error("impossible");
+  // }
+  // return padding === 0 ? pa + pb : pa + Array().fill("0").join("") + pb;
 };
 
 export default function run(input) {
