6d582eb71aead0eec0a159fa34480add6a06be14
zhouyiyan <balzaczyy@gmail.com>
2021-02-04 13:50:55 +0800

    229 slow

--- src/229/index.js
+++ src/229/index.js
@@ -1,11 +1,30 @@
-import { group } from "../utils.js";
-
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
-var majorityElement = function (nums) {
-  return [];
+const majorityElement = function (nums) {
+  const ans = [];
+  nums.sort((a, b) => a - b);
+  if (nums.length > 0) {
+    const threshold = Math.floor(nums.length / 3);
+    let last = nums[0],
+      count = 1;
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i] === last) {
+        count++;
+      } else {
+        if (count > threshold) {
+          ans.push(last);
+        }
+        last = nums[i];
+        count = 1;
+      }
+    }
+    if (count > threshold) {
+      ans.push(last);
+    }
+  }
+  return ans;
 };
 
 export default function run(input) {
