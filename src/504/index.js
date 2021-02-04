4ad23dbc996b341d41455ea03f8df31390f0d117
zhouyiyan <balzaczyy@gmail.com>
2021-02-04 14:32:59 +0800

    504

--- src/504/index.js
+++ src/504/index.js
@@ -3,7 +3,19 @@
  * @return {string}
  */
 const convertToBase7 = function (num) {
-  return "0";
+  if (num === 0) {
+    return "0";
+  } else if (num < 0) {
+    return "-" + convertToBase7(-num);
+  }
+  const ans = [];
+  while (num > 0) {
+    const d = num % 7;
+    ans.push(d);
+    num = Math.floor(num / 7);
+  }
+  ans.reverse();
+  return ans.join("");
 };
 
 export default function run(input) {
