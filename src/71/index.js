f87cd35f69ddde230546a1a1f995b94915f28978
zhouyiyan <balzaczyy@gmail.com>
2021-02-05 19:48:47 +0800

    71 wa

--- src/71/index.js
+++ src/71/index.js
@@ -3,7 +3,75 @@
  * @return {string}
  */
 const simplifyPath = function (path) {
-  return path;
+  const typeSlash = 1,
+    typeWord = 2,
+    typeSingleDot = 3,
+    typeDoubleDots = 4;
+
+  function readSlash(pos) {
+    pos++;
+    while (pos < path.length && path[pos] === "/") {
+      pos++;
+    }
+    return [pos, typeSlash];
+  }
+
+  function readWord(pos) {
+    const start = pos;
+    while (pos < path.length && path[pos].match(/[a-z_\\.]/i)) {
+      pos++;
+    }
+    return [pos, typeWord, path.substring(start, pos)];
+  }
+
+  function readDot(pos) {
+    const start = pos;
+    pos++;
+    while (pos < path.length && path[pos] === ".") {
+      pos++;
+    }
+    if (pos - start === 1) {
+      return [pos, typeSingleDot];
+    } else if (pos - start === 2) {
+      return [pos, typeDoubleDots];
+    }
+    const [posNext, , w] = readWord(pos);
+    return [posNext, typeWord, path.substring(start, pos) + w];
+  }
+
+  function nextToken(pos) {
+    switch (path[pos]) {
+      case "/":
+        return readSlash(pos);
+      case ".":
+        return readDot(pos);
+      default:
+        return readWord(pos);
+    }
+  }
+
+  const ans = [];
+  let i = 0;
+  while (i < path.length) {
+    const [next, t, s] = nextToken(i);
+    switch (t) {
+      case typeSlash:
+      case typeSingleDot:
+        // just skip
+        break;
+      case typeWord:
+        ans.push(s);
+        break;
+      case typeDoubleDots:
+        ans.pop();
+        ans.pop();
+        break;
+      default:
+        throw new Error("impossible");
+    }
+    i = next;
+  }
+  return "/" + ans.join("/");
 };
 
 export default function run(input) {
