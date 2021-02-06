ebbe53dad9415b27a61def6393d8e18d28cd15eb
zhouyiyan <balzaczyy@gmail.com>
2021-02-06 20:04:30 +0800

    497

--- src/497/index.js
+++ src/497/index.js
@@ -4,14 +4,26 @@ import { group } from "../utils.js";
  * @param {number[][]} rects
  */
 const Solution = function (rects) {
-  console.log(rects);
+  this.rects = rects;
+  this.areas = rects.map(([x1, y1, x2, y2]) => (x2 - x1 + 1) * (y2 - y1 + 1));
+  this.total = this.areas.reduce((acc, cur) => acc + cur, 0);
 };
 
 /**
  * @return {number[]}
  */
 Solution.prototype.pick = function () {
-  return [1, 1];
+  let seed = Math.floor(Math.random() * this.total);
+  let i = 0;
+  while (i < this.rects.length && seed >= this.areas[i]) {
+    seed -= this.areas[i];
+    i++;
+  }
+  const [x1, y1, x2] = this.rects[i];
+  const cols = x2 - x1 + 1;
+  const x = x1 + (seed % cols);
+  const y = y1 + Math.floor(seed / cols);
+  return [x, y];
 };
 
 /**
@@ -46,11 +58,16 @@ export default function run(input) {
       const freqs = new Map();
       for (let i = 0; i < ans.length; i++) {
         const [x, y] = ans[i];
+        let found = false;
         for (let j = 0; j < values.length; j++) {
           const [x1, y1, x2, y2] = values[j];
-          if (!inSquare(x, y, x1, y1, x2, y2)) {
-            return false;
+          if (inSquare(x, y, x1, y1, x2, y2)) {
+            found = true;
+            break;
           }
+        }
+        if (!found) {
+          return false;
         }
         const k = `${x}:${y}`;
         freqs.set(k, (freqs.get(k) || 0) + 1);
@@ -61,7 +78,7 @@ export default function run(input) {
       const avg = sum / freqs.size;
       let isOk = true;
       freqs.forEach((v) => {
-        if (Math.abs(v - avg) / avg > 0.05) {
+        if (Math.abs(v - avg) / avg > 0.05 && v - avg > 1) {
           isOk = false;
         }
       });
