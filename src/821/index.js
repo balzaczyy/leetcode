dfb25f45e7addeead19c103cbbef268ced5f5986
zhouyiyan <balzaczyy@gmail.com>
2021-02-07 19:34:48 +0800

    821

--- src/821/index.js
+++ src/821/index.js
@@ -2,11 +2,32 @@ import { group } from "../utils.js";
 
 /**
  * @param {string} s
- * @param {character} c
+ * @param {string} c
  * @return {number[]}
  */
 const shortestToChar = function (s, c) {
-  return [];
+  const ans = Array(s.length);
+  let last = -1;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === c) {
+      last = i;
+      ans[i] = 0;
+    } else if (last >= 0) {
+      ans[i] = i - last;
+    }
+  }
+  last = s.length;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === c) {
+      last = i;
+      ans[i] = 0;
+    } else if (last < s.length) {
+      if (!ans[i] || ans[i] > last - i) {
+        ans[i] = last - i;
+      }
+    }
+  }
+  return ans;
 };
 
 export default function run(input) {
