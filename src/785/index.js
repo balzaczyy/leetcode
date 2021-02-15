68c91c5e1f87871071e170d8465b4fce8e8412a1
zhouyiyan <balzaczyy@gmail.com>
2021-02-15 11:55:01 +0800

    785

--- src/785/index.js
+++ src/785/index.js
@@ -1,10 +1,28 @@
-import { group } from "../utils.js";
-
 /**
  * @param {number[][]} graph
  * @return {boolean}
  */
 const isBipartite = function (graph) {
+  const s1 = new Set();
+  const s2 = new Set();
+  for (let i = 0; i < graph.length; i++) {
+    if (graph[i].length === 1) {
+      continue;
+    }
+    for (let j = 0; j < graph[i].length; j++) {
+      const inS1 = s1.has(i);
+      const inS2 = s2.has(i);
+      if (inS1 && inS2) {
+        return false;
+      }
+      const t = graph[i][j];
+      if (inS1) {
+        s2.add(t);
+      } else {
+        s1.add(t);
+      }
+    }
+  }
   return true;
 };
 
