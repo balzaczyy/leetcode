79af96377a562c583ca0a534788fb06ed964450d
zhouyiyan <balzaczyy@gmail.com>
2021-02-15 17:32:58 +0800

    747 test

---
+++ src/747/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const dominantIndex = function (nums) {
+  return 0;
+};
+
+export default function run(input) {
+  return input.map(JSON.parse).map(dominantIndex).map(String);
+}
