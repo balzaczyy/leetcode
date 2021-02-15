a7c0ca7d0aaa47a416cd37be81ebbdbf2e26209d
zhouyiyan <balzaczyy@gmail.com>
2021-02-15 17:48:24 +0800

    747

--- src/747/index.js
+++ src/747/index.js
@@ -3,7 +3,27 @@
  * @return {number}
  */
 const dominantIndex = function (nums) {
-  return 0;
+  if (nums.length < 2) {
+    return 0;
+  }
+  let max, sec;
+  if (nums[0] > nums[1]) {
+    max = 0;
+    sec = 1;
+  } else {
+    max = 1;
+    sec = 0;
+  }
+  for (let i = 2; i < nums.length; i++) {
+    const v = nums[i];
+    if (v > nums[max]) {
+      sec = max;
+      max = i;
+    } else if (v > nums[sec]) {
+      sec = i;
+    }
+  }
+  return nums[max] >= nums[sec] * 2 ? max : -1;
 };
 
 export default function run(input) {
