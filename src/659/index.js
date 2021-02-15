4f9942bec4fb3d31dbb86c5c7d3dcb83a550932f
zhouyiyan <balzaczyy@gmail.com>
2021-02-15 15:39:17 +0800

    659 WA

--- src/659/index.js
+++ src/659/index.js
@@ -3,6 +3,48 @@
  * @return {boolean}
  */
 const isPossible = function (nums) {
+  function _isPossible(counts) {
+    if (counts.length < 3) {
+      return false;
+    }
+
+    let last = counts[1] - counts[0];
+    const seqs = [[0, last]];
+    for (let i = 2; i < counts.length; i++) {
+      const n = counts[i] - counts[i - 1];
+      if (n === last) {
+        // do nothing
+      } else if (n > last) {
+        seqs.push([i, n - last]);
+      } else {
+        // n < last
+        let count = last - n;
+        while (count > 0 && seqs.length > 0) {
+          if (i - seqs[0][0] < 3) {
+            return false;
+          }
+          if (seqs[0][1] > count) {
+            seqs[0][1] -= count;
+            count = 0;
+          } else {
+            count -= seqs[0][1];
+            seqs.shift();
+          }
+        }
+        if (count > 0) {
+          return false;
+        }
+      }
+    }
+    while (seqs.length > 0) {
+      const t = seqs.shift();
+      if (nums.length - t[0] < 3) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   function next(offset) {
     const base = nums[offset];
     offset++;
@@ -11,57 +53,21 @@ const isPossible = function (nums) {
     }
     return offset;
   }
-  const counts = [0];
+  let counts = [0];
   for (let i = next(0); i < nums.length; i = next(i)) {
     const last = nums[counts[counts.length - 1]];
     const cur = nums[i];
-    if (cur !== last + 1) {
-      return false;
-    }
     counts.push(i);
-  }
-  counts.push(nums.length);
-  // console.log(counts);
-
-  if (counts.length < 3) {
-    return false;
-  }
-
-  let last = counts[1];
-  const seqs = [[0, last]];
-  for (let i = 2; i < counts.length; i++) {
-    const n = counts[i] - counts[i - 1];
-    if (n === last) {
-      // do nothing
-    } else if (n > last) {
-      seqs.push([i, n - last]);
-    } else {
-      // n < last
-      let count = last - n;
-      while (count > 0 && seqs.length > 0) {
-        if (i - seqs[0][0] < 3) {
-          return false;
-        }
-        if (seqs[0][1] > count) {
-          seqs[0][1] -= count;
-          count = 0;
-        } else {
-          count -= seqs[0][1];
-          seqs.shift();
-        }
-      }
-      if (count > 0) {
+    if (cur !== last + 1) {
+      const ans = _isPossible(counts);
+      if (!ans) {
         return false;
       }
+      counts = [i];
     }
   }
-  while (seqs.length > 0) {
-    const t = seqs.shift();
-    if (nums.length - t[0] < 3) {
-      return false;
-    }
-  }
-  return true;
+  counts.push(nums.length);
+  return _isPossible(counts);
 };
 
 export default function run(input) {
