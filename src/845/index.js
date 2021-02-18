620f2da608cbe3577f7d1a010386faa163b49e95
zhouyiyan <balzaczyy@gmail.com>
2021-02-18 10:50:01 +0800

    845 WA

--- src/845/index.js
+++ src/845/index.js
@@ -3,7 +3,35 @@
  * @return {number}
  */
 const longestMountain = function (arr) {
-  return 0;
+  let start = 0,
+    peak = 0,
+    end = 0,
+    max = 0;
+  arr.push(Number.MAX_SAFE_INTEGER);
+  for (let i = 1; i < arr.length; i++) {
+    if (end <= peak) {
+      // ascending
+      if (arr[i] > arr[i - 1]) {
+        peak = i;
+      } else if (arr[i] === arr[i - 1]) {
+        start = peak = end = i;
+      } else {
+        end = i;
+      }
+    } else {
+      // descending
+      if (arr[i] > arr[i - 1]) {
+        max = Math.max(max, end - start + 1);
+        start = end;
+        peak = i;
+      } else if (arr[i] === arr[i - 1]) {
+        start = peak = end = i;
+      } else {
+        end = i;
+      }
+    }
+  }
+  return max < 3 ? 0 : max;
 };
 
 export default function run(input) {
