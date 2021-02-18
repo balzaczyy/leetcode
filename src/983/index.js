2ca98cb0764772a98e874e3aa386bfd052c25a94
zhouyiyan <balzaczyy@gmail.com>
2021-02-18 13:29:32 +0800

    983

--- src/983/index.js
+++ src/983/index.js
@@ -6,11 +6,22 @@ import { group } from "../utils.js";
  * @return {number}
  */
 const mincostTickets = function (days, costs) {
-  return 0;
+  const cost = [];
+  for (let i = 0, j = 0; i < 365 && j < days.length; i++) {
+    if (days[j] === i + 1) {
+      j++;
+      cost[i] = (cost[i - 1] || 0) + costs[0];
+      cost[i] = Math.min(cost[i], (cost[i - 7] || 0) + costs[1]);
+      cost[i] = Math.min(cost[i], (cost[i - 30] || 0) + costs[2]);
+    } else {
+      cost[i] = cost[i - 1];
+    }
+  }
+  return cost[cost.length - 1];
 };
 
 export default function run(input) {
-  return group(input.map(Number), 2)
+  return group(input.map(JSON.parse), 2)
     .map(([days, costs]) => mincostTickets(days, costs))
     .map(String);
 }
