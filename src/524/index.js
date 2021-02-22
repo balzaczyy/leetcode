ee94248e8997e14824b1c2818dc742a89383036a
zhouyiyan <balzaczyy@gmail.com>
2021-02-22 18:25:05 +0800

    524

--- src/524/index.js
+++ src/524/index.js
@@ -6,9 +6,31 @@ import { group } from "../utils.js";
  * @return {string}
  */
 const findLongestWord = function (s, d) {
-  return s;
+  const candidates = d.filter((v) => v.length <= s.length);
+  candidates.sort((a, b) => {
+    if (a.length !== b.length) {
+      return b.length - a.length;
+    }
+    return a.localeCompare(b);
+  });
+
+  return (
+    candidates.find((v) => {
+      let i = 0,
+        j = 0;
+      while (i < s.length && j < v.length) {
+        if (s[i] === v[j]) {
+          j++;
+        }
+        i++;
+      }
+      return j === v.length;
+    }) || ""
+  );
 };
 
 export default function run(input) {
-  return group(input.map(JSON.parse), 2).map(([s, d]) => findLongestWord(s, d));
+  return group(input.map(JSON.parse), 2)
+    .map(([s, d]) => findLongestWord(s, d))
+    .map((v) => JSON.stringify(v));
 }
