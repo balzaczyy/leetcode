f91a5adca320eeb13be4439d85e6110266c20aaa
zhouyiyan <balzaczyy@gmail.com>
2021-02-24 18:27:16 +0800

    845

--- src/856/index.js
+++ src/856/index.js
@@ -3,7 +3,29 @@
  * @return {number}
  */
 const scoreOfParentheses = function (S) {
-  return 1;
+  const stack = [];
+  for (let i = 0; i < S.length; i++) {
+    switch (S[i]) {
+      case "(":
+        stack.push(0);
+        break;
+      case ")":
+        if (stack[stack.length - 1] === 0) {
+          stack[stack.length - 1] = 1;
+        } else {
+          let sum = 0;
+          while (stack[stack.length - 1] > 0) {
+            sum += stack.pop();
+            if (stack.length === 0) {
+              throw new Error("bad string");
+            }
+          }
+          stack[stack.length - 1] = sum * 2;
+        }
+        break;
+    }
+  }
+  return stack.reduce((acc, cur) => acc + cur);
 };
 
 export default function run(input) {
