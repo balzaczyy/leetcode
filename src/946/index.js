b6ceb292d96b01743de4f553fa6d37e6fedc9828
zhouyiyan <balzaczyy@gmail.com>
2021-02-27 10:15:44 +0800

    946

--- src/946/index.js
+++ src/946/index.js
@@ -6,7 +6,26 @@ import { group } from "../utils.js";
  * @return {boolean}
  */
 const validateStackSequences = function (pushed, popped) {
-  return true;
+  const stack = [];
+  let pos = 0;
+  for (const v of pushed) {
+    if (v === popped[pos]) {
+      pos++;
+      while (stack.length > 0 && stack[stack.length - 1] === popped[pos]) {
+        stack.pop();
+        pos++;
+      }
+    } else {
+      stack.push(v);
+    }
+  }
+  while (stack.length > 0) {
+    if (stack.pop() !== popped[pos]) {
+      return false;
+    }
+    pos++;
+  }
+  return pos === popped.length;
 };
 
 export default function run(input) {
