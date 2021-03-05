a39c7fe5df085eea7d51552d962425a5114302dc
zhouyiyan <balzaczyy@gmail.com>
2021-03-05 12:55:10 +0800

    160 test

---
+++ src/160/index.js
@@ -0,0 +1,44 @@
+import { group } from "../utils.js";
+import { arrayToListNode } from "../23/index.js";
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+const getIntersectionNode = function (headA, headB) {
+  return headA;
+};
+
+export default function run(input) {
+  return group(input.map(JSON.parse), 5)
+    .map(([intersectVal, listA, listB, skipA, skipB]) => {
+      const hA = arrayToListNode(listA);
+      const hB = arrayToListNode(listB);
+      let t1 = hA;
+      while (skipA > 1) {
+        t1 = t1.next;
+        skipA--;
+      }
+      let t2 = hB;
+      while (skipB > 1) {
+        t2 = t2.next;
+        skipB--;
+      }
+      if (t1.next.val !== intersectVal || t2.next.val !== intersectVal) {
+        throw new Error("impossible");
+      }
+      t2.next = t1.next;
+      return [hA, hB];
+    })
+    .map(([headA, headB]) => getIntersectionNode(headA, headB))
+    .map((head) => String(head.val));
+}
