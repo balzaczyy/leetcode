aea3448298702b6d94234a0129a5af0e2ff22c56
zhouyiyan <balzaczyy@gmail.com>
2021-03-07 15:26:26 +0800

    645

--- src/645/index.js
+++ src/645/index.js
@@ -4,10 +4,13 @@
  */
 const findErrorNums = function (nums) {
   let dup = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== i + 1) {
+  for (let i = 0; i < nums.length; ) {
+    if (nums[i] === i + 1) {
+      i++;
+    } else {
       if (nums[nums[i] - 1] === nums[i]) {
         dup = nums[i];
+        i++;
       } else {
         const t = nums[i];
         nums[i] = nums[t - 1];
@@ -15,7 +18,7 @@ const findErrorNums = function (nums) {
       }
     }
   }
-  console.log(nums);
+  // console.log(nums);
   const sum = nums.reduce((acc, cur) => acc + cur);
   const target = ((nums.length + 1) * nums.length) / 2;
   const x = target - sum + dup;
