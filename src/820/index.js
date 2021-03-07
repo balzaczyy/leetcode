c3d40a806982f9db7982afecce7211e756e58f5c
zhouyiyan <balzaczyy@gmail.com>
2021-03-07 09:31:24 +0800

    820

--- src/820/index.js
+++ src/820/index.js
@@ -3,7 +3,21 @@
  * @return {number}
  */
 const minimumLengthEncoding = function (words) {
-  return 0;
+  words.sort((a, b) => a.length - b.length);
+  let ans = 0;
+  for (let i = 0; i < words.length; i++) {
+    let isUnique = true;
+    for (let j = i + 1; j < words.length; j++) {
+      if (words[j].endsWith(words[i])) {
+        isUnique = false;
+        break;
+      }
+    }
+    if (isUnique) {
+      ans += words[i].length + 1;
+    }
+  }
+  return ans;
 };
 
 export default function run(input) {
