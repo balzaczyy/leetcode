ed8494e87194e1b1ed9e555912ee364c5e63224f
zhouyiyan <balzaczyy@gmail.com>
2021-03-08 17:40:41 +0800

    1332 WA

--- src/1332/index.js
+++ src/1332/index.js
@@ -3,7 +3,27 @@
  * @return {number}
  */
 const removePalindromeSub = function (s) {
-  return 0;
+  const ps = Array(s.length);
+  for (let i = 0; i < s.length; i++) {
+    ps[i] = 1;
+    for (let j = i - 1; j >= 0; j--) {
+      if (j === i - 1) {
+        if (s[j] === s[i]) {
+          ps[j] = 2;
+        }
+      } else {
+        if (ps[j + 1] + j + 1 === i && s[j] === s[i]) {
+          ps[j] = ps[j + 1] + 2;
+        }
+      }
+    }
+  }
+  // console.log(ps);
+  let ans = 0;
+  for (let i = 0; i < s.length; i += ps[i]) {
+    ans++;
+  }
+  return ans;
 };
 
 export default function run(input) {
