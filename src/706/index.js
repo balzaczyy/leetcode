2e58f4d5c93736a737cef29623f76fb1c29d0d9b
zhouyiyan <balzaczyy@gmail.com>
2021-03-08 20:54:23 +0800

    706

--- src/706/index.js
+++ src/706/index.js
@@ -3,7 +3,11 @@ import { group } from "../utils.js";
 /**
  * Initialize your data structure here.
  */
-const MyHashMap = function () {};
+const MyHashMap = function () {
+  const seed = 97;
+  this.hash = (n) => n % seed;
+  this.store = Array(seed);
+};
 
 /**
  * value will always be non-negative.
@@ -11,21 +15,53 @@ const MyHashMap = function () {};
  * @param {number} value
  * @return {void}
  */
-MyHashMap.prototype.put = function (key, value) {};
+MyHashMap.prototype.put = function (key, value) {
+  const idx = this.hash(key);
+  const v = this.store[idx];
+  if (v) {
+    const found = v.find((v) => v.key === key);
+    if (found) {
+      found.value = value;
+    } else {
+      v.push({ key, value });
+    }
+  } else {
+    this.store[idx] = [{ key, value }];
+  }
+};
 
 /**
  * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
  * @param {number} key
  * @return {number}
  */
-MyHashMap.prototype.get = function (key) {};
+MyHashMap.prototype.get = function (key) {
+  const idx = this.hash(key);
+  const v = this.store[idx];
+  if (v) {
+    const found = v.find((v) => v.key === key);
+    if (found) {
+      return found.value;
+    }
+  }
+  return -1;
+};
 
 /**
  * Removes the mapping of the specified value key if this map contains a mapping for the key
  * @param {number} key
  * @return {void}
  */
-MyHashMap.prototype.remove = function (key) {};
+MyHashMap.prototype.remove = function (key) {
+  const idx = this.hash(key);
+  const v = this.store[idx];
+  if (v) {
+    const found = v.findIndex((v) => v.key === key);
+    if (found >= 0) {
+      v.splice(found, 1);
+    }
+  }
+};
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
@@ -48,13 +84,13 @@ export default function run(input) {
             ans.push(null);
             break;
           case "put":
-            ans.push(obj.put());
+            ans.push(obj.put(...param));
             break;
           case "get":
-            ans.push(obj.get());
+            ans.push(obj.get(...param));
             break;
           case "remove":
-            ans.push(obj.remove());
+            ans.push(obj.remove(...param));
             break;
         }
       }
