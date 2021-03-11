a3ff8e45f15888ef152d3dcdbe68589557ed2e9c
zhouyiyan <balzaczyy@gmail.com>
2021-03-11 20:32:03 +0800

    322

--- src/322/index.js
+++ src/322/index.js
@@ -6,14 +6,27 @@ import { group } from "../utils.js";
  * @return {number}
  */
 const coinChange = function (coins, amount) {
-  return -1;
+  const ans = Array(amount + 1);
+  ans[0] = 0;
+  for (let i = 1; i <= amount; i++) {
+    let min = -1;
+    for (let j = 0; j < coins.length; j++) {
+      const last = i - coins[j];
+      if (last === 0 || ans[last] > 0) {
+        if (min < 0) {
+          min = ans[last] + 1;
+        } else {
+          min = Math.min(min, ans[last] + 1);
+        }
+      }
+    }
+    ans[i] = min;
+  }
+  return ans[amount];
 };
 
 export default function run(input) {
-  return group(
-    input.map((v) => Number(v)),
-    2
-  )
+  return group(input.map(JSON.parse), 2)
     .map(([coins, amount]) => coinChange(coins, amount))
     .map(String);
 }
