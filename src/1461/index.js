c648269e99908ee6ff9e158938909d4f697265e8
zhouyiyan <balzaczyy@gmail.com>
2021-03-13 08:53:15 +0800

    1461 WA

--- src/1461/index.js
+++ src/1461/index.js
@@ -6,6 +6,32 @@ import { group } from "../utils.js";
  * @return {boolean}
  */
 const hasAllCodes = function (s, k) {
+  const numCodecs = 1 << k;
+  if (s.length < numCodecs - 1 + k) {
+    return false;
+  }
+
+  const pool = new Set();
+  let seed = 0;
+  for (let i = 0; i < k; i++) {
+    seed = (seed << 1) | Number(s[i]);
+  }
+  pool.add(seed);
+  // console.log(s.substring(0, k), seed);
+
+  const mask = (1 << k) - 1;
+  for (let i = k; i < s.length; i++) {
+    if (s.length - i + pool.size < numCodecs) {
+      return false; // fast fail
+    }
+    if (s.length === numCodecs) {
+      return true; // fast quit
+    }
+
+    seed = ((seed << 1) | Number(s[i])) & mask;
+    pool.add(seed);
+  }
+
   return true;
 };
 
