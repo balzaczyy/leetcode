6050b79afafcf1e26857c2edcda1135a575440f0
zhouyiyan <balzaczyy@gmail.com>
2021-03-13 19:40:08 +0800

    823 test

---
+++ src/823/index.js
@@ -0,0 +1,13 @@
+import { group } from "../utils.js";
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const numFactoredBinaryTrees = function (arr) {
+  return 0;
+};
+
+export default function run(input) {
+  return input.map(JSON.parse).map(numFactoredBinaryTrees).map(String);
+}
