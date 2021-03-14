fe75a0dbfd3df3e33874b30af7bedf93f79a94dd
zhouyiyan <balzaczyy@gmail.com>
2021-03-14 16:26:18 +0800

    1721 - AC system has bug

--- src/1721/index.js
+++ src/1721/index.js
@@ -1,4 +1,5 @@
 import { group } from "../utils.js";
+import { arrayToListNode, listNodeToArray } from "../23/index.js";
 
 /**
  * Definition for singly-linked list.
@@ -14,11 +15,34 @@ import { group } from "../utils.js";
  * @return {ListNode}
  */
 const swapNodes = function (head, k) {
+  let count = 0;
+  let t = head;
+  for (let i = 0; i < k - 1; i++) {
+    t = t.next;
+    count++;
+  }
+  const p = t;
+  while (t != null) {
+    t = t.next;
+    count++;
+  }
+  t = head;
+  for (let i = 0; i < count - k; i++) {
+    t = t.next;
+  }
+  if (p !== t) {
+    const temp = p.val;
+    p.val = t.val;
+    t.val = temp;
+  }
+
   return head;
 };
 
 export default function run(input) {
   return group(input.map(JSON.parse), 2)
+    .map(([a, b]) => [arrayToListNode(a), b])
     .map(([head, k]) => swapNodes(head, k))
+    .map((v) => listNodeToArray(v))
     .map((v) => JSON.stringify(v));
 }
