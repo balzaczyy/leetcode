ee13bbb588cb97e35583224fc5c23d2e9f514a71
zhouyiyan <balzaczyy@gmail.com>
2021-03-17 21:11:05 +0800

    714

--- src/714/index.js
+++ src/714/index.js
@@ -6,7 +6,24 @@ import { group } from "../utils.js";
  * @return {number}
  */
 const maxProfit = function (prices, fee) {
-  return 0;
+  const days = prices.length;
+  const profits = Array(days);
+  profits[0] = [0, 0];
+  for (let i = 1; i < days; i++) {
+    let max = profits[i - 1][0];
+    for (let j = i - 1; j >= 0; j--) {
+      const limit = prices[i] - fee;
+      if (profits[j][1] + limit < profits[i - 1][0]) {
+        break;
+      }
+      const p = prices[i] - prices[j] - fee;
+      if (p > 0) {
+        max = Math.max(max, profits[j][1] + p); // transaction i->j
+      }
+    }
+    profits[i] = [max, Math.max(profits[i - 1][0], profits[i - 1][1])];
+  }
+  return profits[days - 1][0];
 };
 
 export default function run(input) {
