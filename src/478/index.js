ce6569db58a55e0d9d4fcfd10833c3c24ab8a083
zhouyiyan <balzaczyy@gmail.com>
2021-03-17 18:35:46 +0800

    478

--- src/478/index.js
+++ src/478/index.js
@@ -1,18 +1,30 @@
 import { group } from "../utils.js";
-import { Iterator } from "../284/index.js";
 
 /**
  * @param {number} radius
  * @param {number} x_center
  * @param {number} y_center
  */
-var Solution = function (radius, x_center, y_center) {};
+const Solution = function (radius, x_center, y_center) {
+  this.radius = radius;
+  this.x_center = x_center;
+  this.y_center = y_center;
+};
 
 /**
  * @return {number[]}
  */
 Solution.prototype.randPoint = function () {
-  return [0, 0];
+  const x = Math.random() * this.radius * 2 + this.x_center - this.radius;
+  const y = Math.random() * this.radius * 2 + this.y_center - this.radius;
+  const d = Math.sqrt(
+    Math.pow(Math.abs(x - this.x_center), 2) +
+      Math.pow(Math.abs(y - this.y_center), 2)
+  );
+  if (d <= this.radius) {
+    return [x, y];
+  }
+  return this.randPoint();
 };
 
 /**
@@ -24,21 +36,20 @@ Solution.prototype.randPoint = function () {
 export default function run(input) {
   return group(input.map(JSON.parse), 2)
     .map(([commands, params]) => {
-      const ans = [];
       let obj = {};
       for (let i = 0; i < commands.length; i++) {
         const param = params[i];
         switch (commands[i]) {
           case "Solution":
+            console.log(param);
             obj = new Solution(...param);
-            ans.push(null);
             break;
           case "randPoint":
-            ans.push(obj.randPoint());
+            console.log(obj.randPoint());
             break;
         }
       }
-      return ans;
+      return true;
     })
-    .map((v) => JSON.stringify(v));
+    .map(String);
 }
