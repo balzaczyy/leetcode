91fc5c27208976c2c1f84947a2c309382d29f0cb
zhouyiyan <balzaczyy@gmail.com>
2021-03-18 21:25:20 +0800

    376

--- src/376/index.js
+++ src/376/index.js
@@ -1,11 +1,31 @@
-import { group } from "../utils.js";
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
 const wiggleMaxLength = function (nums) {
-  return 0;
+  const len = nums.length;
+  const ans = Array(len);
+  for (let i = 0; i < len; i++) {
+    let h = 1,
+      l = 1;
+    for (let j = 0; j < i; j++) {
+      if (j === 0) {
+        if (nums[i] > nums[j]) {
+          h = Math.max(h, ans[j][0] + 1);
+        } else if (nums[i] < nums[j]) {
+          l = Math.max(l, ans[j][1] + 1);
+        }
+      } else {
+        if (nums[i] > nums[j] && nums[j] < nums[j - 1]) {
+          h = Math.max(h, ans[j][1] + 1);
+        } else if (nums[i] < nums[j] && nums[j] > nums[j - 1]) {
+          l = Math.max(l, ans[j][0] + 1);
+        }
+      }
+    }
+    ans[i] = [h, l];
+  }
+  return Math.max(ans[len - 1][0], ans[len - 1][1]);
 };
 
 export default function run(input) {
