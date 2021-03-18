376502efba7c380f40ad100a56aa2084bfe545d6
zhouyiyan <balzaczyy@gmail.com>
2021-03-18 20:42:23 +0800

    376 test

---
+++ src/376/index.js
@@ -0,0 +1,13 @@
+import { group } from "../utils.js";
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const wiggleMaxLength = function (nums) {
+  return 0;
+};
+
+export default function run(input) {
+  return input.map(JSON.parse).map(wiggleMaxLength).map(String);
+}
