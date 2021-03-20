7d0c694e8a354db601d718d3a1617d5a3a97d734
zhouyiyan <balzaczyy@gmail.com>
2021-03-20 12:54:15 +0800

    841

--- src/841/index.js
+++ src/841/index.js
@@ -3,7 +3,21 @@
  * @return {boolean}
  */
 const canVisitAllRooms = function (rooms) {
-  return true;
+  const visited = new Set();
+  const q = [0];
+  visited.add(0);
+  const len = rooms.length;
+  while (q.length > 0 && visited.size < len) {
+    const t = q.shift();
+    for (let i = 0; i < rooms[t].length; i++) {
+      const next = rooms[t][i];
+      if (!visited.has(next)) {
+        visited.add(next);
+        q.push(next);
+      }
+    }
+  }
+  return visited.size === len;
 };
 
 export default function run(input) {
