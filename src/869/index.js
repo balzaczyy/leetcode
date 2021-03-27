6155cf9e4afc0ce49b480d1572b32ce730bb06f7
zhouyiyan <balzaczyy@gmail.com>
2021-03-27 20:50:47 +0800

    869

--- src/869/index.js
+++ src/869/index.js
@@ -3,7 +3,22 @@
  * @return {boolean}
  */
 const reorderedPowerOf2 = function (n) {
-  return false;
+  const id = (n) => {
+    const counts = Array(10).fill(0);
+    while (n > 0) {
+      const d = n % 10;
+      counts[d]++;
+      n = (n - d) / 10;
+    }
+    return counts.join("-");
+  };
+  const keys = new Set();
+  const limit = Math.min(1000000000, n * 10);
+  for (let i = 1; i <= limit; i = i << 1) {
+    keys.add(id(i));
+  }
+  // console.log(keys);
+  return keys.has(id(n));
 };
 
 export default function run(input) {
