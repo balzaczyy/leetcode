6f23d0ace1a06c8d34c787035652f6dd05d1e06b
zhouyiyan <balzaczyy@gmail.com>
2021-03-27 20:33:06 +0800

    966 WA

--- src/966/index.js
+++ src/966/index.js
@@ -6,7 +6,39 @@ import { group } from "../utils.js";
  * @return {string[]}
  */
 const spellchecker = function (wordlist, queries) {
-  return [];
+  const sensitive = new Map();
+  const insensitive = new Map();
+  const fuzz = new Map();
+  const toFuzzKey = (v) => v.replace(/[aeiou]/gi, "-");
+  wordlist.forEach((v) => {
+    sensitive.set(v, v);
+    if (!insensitive.has(v.toLowerCase())) {
+      insensitive.set(v.toLowerCase(), v);
+    }
+    const fuzzKey = toFuzzKey(v);
+    // console.log(v, fuzzKey);
+    if (!fuzz.has(fuzzKey)) {
+      fuzz.set(fuzzKey, v);
+    }
+  });
+  // console.log(sensitive, insensitive, fuzz);
+  return queries.map((v) => {
+    if (sensitive.has(v)) {
+      return sensitive.get(v);
+    }
+    if (insensitive.has(v.toLowerCase())) {
+      return insensitive.get(v.toLowerCase());
+    }
+    const fuzzKey = toFuzzKey(v);
+    if (fuzz.has(fuzzKey)) {
+      const one = fuzz.get(fuzzKey);
+      if (insensitive.has(one)) {
+        return insensitive.get(one);
+      }
+      return one;
+    }
+    return "";
+  });
 };
 
 export default function run(input) {
