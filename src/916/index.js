d7c03e074e66ef472fed9c7f0011635156792eb5
zhouyiyan <balzaczyy@gmail.com>
2021-03-27 08:57:21 +0800

    916

--- src/916/index.js
+++ src/916/index.js
@@ -6,7 +6,31 @@ import { group } from "../utils.js";
  * @return {string[]}
  */
 const wordSubsets = function (A, B) {
-  return A;
+  const sig = B.reduce((acc, cur) => {
+    const f = new Map();
+    for (const ch of cur) {
+      f.set(ch, 1 + (f.get(ch) || 0));
+    }
+    for (const e of f) {
+      const [ch, count] = e;
+      acc.set(ch, Math.max(count, acc.get(ch) || 0));
+    }
+    return acc;
+  }, new Map());
+  // console.log(sig);
+  return A.filter((a) => {
+    const f = new Map();
+    for (const ch of a) {
+      f.set(ch, 1 + (f.get(ch) || 0));
+    }
+    for (const e of sig) {
+      const [ch, count] = e;
+      if (!f.has(ch) || f.get(ch) < count) {
+        return false;
+      }
+    }
+    return true;
+  });
 };
 
 export default function run(input) {
