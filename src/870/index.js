ff7ce900d508463ccb62c1db0cac0480b1afc50a
zhouyiyan <balzaczyy@gmail.com>
2021-03-27 16:55:48 +0800

    870 WA

--- src/870/index.js
+++ src/870/index.js
@@ -6,7 +6,36 @@ import { group } from "../utils.js";
  * @return {number[]}
  */
 const advantageCount = function (A, B) {
-  return A;
+  A.sort((a, b) => a - b);
+  /**
+   * find the lowest number that is larger than v
+   * @param {number} v
+   * @param {number} from
+   * @param {number} to
+   * @return {number}
+   */
+  function search(v, from, to) {
+    while (A[from] < 0) from++;
+    while (A[to] < 0) to--;
+    if (v < A[from] || v >= A[to]) {
+      const t = A[from];
+      A[from] = -1;
+      return t;
+    }
+    if (from + 1 === to) {
+      const t = A[to];
+      A[to] = -1;
+      return t;
+    }
+    // now v in [A[from],A[to]) and at least 3 values do binary search
+    const mid = Math.floor((from + to) / 2);
+    const midV = A[mid];
+    if (v >= midV) {
+      return search(v, mid, to);
+    }
+    return search(v, from, mid);
+  }
+  return B.map((v) => search(v, 0, A.length - 1));
 };
 
 export default function run(input) {
