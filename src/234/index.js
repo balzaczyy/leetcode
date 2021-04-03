30619cff4657658e10a8ef990e5eebb4307f871a
zhouyiyan <balzaczyy@gmail.com>
2021-04-03 14:21:46 +0800

    234

--- src/234/index.js
+++ src/234/index.js
@@ -13,7 +13,39 @@ import { arrayToListNode } from "../23/index.js";
  * @return {boolean}
  */
 const isPalindrome = function (head) {
-  return false;
+  let count = 0;
+  for (let t = head; t != null; t = t.next) {
+    count++;
+  }
+  if (count === 1) {
+    return true;
+  }
+
+  let mid = head,
+    last = null;
+  for (let i = 0; i < count / 2; i++) {
+    const t = mid;
+    mid = mid.next;
+    t.next = last;
+    last = t;
+  }
+  if (count % 2 === 1) {
+    last = last.next;
+  }
+
+  function match(a, b) {
+    if (!a && !b) {
+      return true;
+    }
+    if ((!a && b) || (a && !b)) {
+      return false;
+    }
+    if (a.val !== b.val) {
+      return false;
+    }
+    return match(a.next, b.next);
+  }
+  return match(mid, last);
 };
 
 export default function run(input) {
