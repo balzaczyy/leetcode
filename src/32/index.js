edb8c19d89b75ded24ff05c6dcd33d996a501226
zhouyiyan <balzaczyy@gmail.com>
2021-04-03 21:34:40 +0800

    32 WA

--- src/32/index.js
+++ src/32/index.js
@@ -3,7 +3,32 @@
  * @return {number}
  */
 const longestValidParentheses = function (s) {
-  return 0;
+  /**
+   * @param {string} s
+   * @param {number} offset
+   * @return number
+   */
+  function search(s, offset = 0) {
+    offset = s.indexOf("(", offset);
+    if (offset < 0) {
+      return 0;
+    }
+
+    const stack = [];
+    let res = 0;
+    for (let i = offset; i < s.length; i++) {
+      if (s[i] === "(") {
+        stack.push("(");
+      } else if (stack.length === 0 || stack[stack.length - 1] !== "(") {
+        return Math.max(res, search(s, i + 1));
+      } else {
+        stack.pop();
+        res += 2;
+      }
+    }
+    return res;
+  }
+  return search(s, 0);
 };
 
 export default function run(input) {
