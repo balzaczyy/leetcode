9f5f191048d53c77cf0e0a8c70b2c4ff61bfa8eb
zhouyiyan <balzaczyy@gmail.com>
2021-04-05 20:40:45 +0800

    775

--- src/775/index.js
+++ src/775/index.js
@@ -1,11 +1,26 @@
-import { group } from "../utils.js";
-
 /**
  * @param {number[]} A
  * @return {boolean}
  */
 const isIdealPermutation = function (A) {
-  return false;
+  const all = new Set();
+  for (let i = 0; i < A.length; i++) {
+    all.add(A[i]);
+    if (i > 0 && !all.has(i - 1)) {
+      return false;
+    }
+  }
+  for (let i = 2; i < A.length; i += 2) {
+    if (A[i - 2] > A[i]) {
+      return false;
+    }
+  }
+  for (let i = 3; i < A.length; i += 2) {
+    if (A[i - 2] > A[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 export default function run(input) {
