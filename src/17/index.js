f6c6a3e440313cb3fac4e33378ae8e3fb8bd0357
zhouyiyan <balzaczyy@gmail.com>
2021-04-08 22:26:28 +0800

    17

--- src/17/index.js
+++ src/17/index.js
@@ -3,7 +3,34 @@
  * @return {string[]}
  */
 const letterCombinations = function (digits) {
-  return [];
+  const dict = [
+    [],
+    [],
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"],
+    ["j", "k", "l"],
+    ["m", "n", "o"],
+    ["p", "q", "r", "s"],
+    ["t", "u", "v"],
+    ["w", "x", "y", "z"],
+  ];
+  const ans = [];
+  function fill(combo, pos) {
+    if (pos === digits.length) {
+      if (combo.length > 0) {
+        ans.push(combo.join(""));
+      }
+      return;
+    }
+    for (const ch of dict[Number(digits[pos])]) {
+      combo.push(ch);
+      fill(combo, pos + 1);
+      combo.pop();
+    }
+  }
+  fill([], 0);
+  return ans;
 };
 
 export default function run(input) {
