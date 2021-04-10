6b38f7a26d6be625c9ea4ba1a288e6e23a42b836
zhouyiyan <balzaczyy@gmail.com>
2021-04-10 08:53:38 +0800

    953 WA

--- src/953/index.js
+++ src/953/index.js
@@ -9,9 +9,33 @@ const isAlienSorted = function (words, order) {
   if (words.length <= 1) {
     return true;
   }
-  return false;
+  const rlt = new Map();
+  for (let i = 0; i < order.length; i++) {
+    rlt.set(order[i], i);
+  }
+  function compare(a, b) {
+    for (let j = 0; j < a.length && j < b.length; j++) {
+      const diff = rlt.get(a) - rlt.get(b);
+      if (diff < 0) {
+        return -1;
+      }
+      if (diff > 0) {
+        return 1;
+      }
+    }
+    return a.length > b.length ? 1 : 0;
+  }
+  for (let i = 0; i < words.length - 1; i++) {
+    const res = compare(words[i], words[i + 1]);
+    if (res > 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 export default function run(input) {
-  return group(input.map(JSON.parse), 2).map(isAlienSorted).map(String);
+  return group(input.map(JSON.parse), 2)
+    .map(([words, order]) => isAlienSorted(words, order))
+    .map(String);
 }
