1c7600103ff7d5cf52c49f4cc0897720a564f24c
zhouyiyan <balzaczyy@gmail.com>
2021-04-12 19:30:06 +0800

    526 WA

--- src/526/index.js
+++ src/526/index.js
@@ -6,7 +6,45 @@ import { group } from "../utils.js";
  * @return {number[]}
  */
 const constructArray = function (n, k) {
-  return [];
+  const perm = Array(n);
+  const used = new Set();
+  function search(offset, diff) {
+    if (offset === n) {
+      return true;
+    }
+    diff = Math.max(1, diff);
+
+    let next = perm[offset - 1] + diff;
+    if (next <= n && !used.has(next)) {
+      perm[offset] = next;
+      used.add(next);
+      if (search(offset + 1, diff - 1)) {
+        return true;
+      }
+      used.remove(next);
+    }
+
+    next = Math.abs(perm[offset - 1] - diff);
+    if (next <= n) {
+      perm[offset] = next;
+      used.add(next);
+      if (search(offset + 1, diff - 1)) {
+        return true;
+      }
+      used.remove(next);
+    }
+
+    return false;
+  }
+  for (let i = 1; i <= n; i++) {
+    perm[0] = i;
+    used.add(i);
+    if (search(1, k)) {
+      break;
+    }
+    used.remove(i);
+  }
+  return perm;
 };
 
 export default function run(input) {
