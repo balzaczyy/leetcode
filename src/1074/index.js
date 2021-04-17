224728b7e8a24ee74b5af7391296a0e96d63fa4a
zhouyiyan <balzaczyy@gmail.com>
2021-04-17 15:36:59 +0800

    1074

--- src/1074/index.js
+++ src/1074/index.js
@@ -5,8 +5,39 @@ import { group } from "../utils.js";
  * @param {number} target
  * @return {number}
  */
-var numSubmatrixSumTarget = function (matrix, target) {
-  return 0;
+const numSubmatrixSumTarget = function (matrix, target) {
+  const rowLen = matrix.length;
+  const colLen = matrix[0].length;
+  const sum = Array(rowLen);
+  for (let i = 0; i < rowLen; i++) {
+    sum[i] = Array(colLen);
+    for (let j = 0; j < colLen; j++) {
+      sum[i][j] =
+        (i === 0 ? 0 : sum[i - 1][j]) +
+        (j === 0 ? 0 : sum[i][j - 1]) -
+        (i === 0 || j === 0 ? 0 : sum[i - 1][j - 1]) +
+        matrix[i][j];
+    }
+  }
+  // console.log(sum);
+  const subSum = (i, j, k, l) =>
+    sum[k][l] -
+    (i === 0 ? 0 : sum[i - 1][l]) -
+    (j === 0 ? 0 : sum[k][j - 1]) +
+    (i === 0 || j === 0 ? 0 : sum[i - 1][j - 1]);
+  let ans = 0;
+  for (let i = 0; i < rowLen; i++) {
+    for (let j = 0; j < colLen; j++) {
+      for (let k = i; k < rowLen; k++) {
+        for (let l = j; l < colLen; l++) {
+          if (subSum(i, j, k, l) === target) {
+            ans++;
+          }
+        }
+      }
+    }
+  }
+  return ans;
 };
 
 export default function run(input) {
