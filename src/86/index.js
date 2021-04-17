1353b39b13a0cd910281bf2737a72d51bbb77f6e
zhouyiyan <balzaczyy@gmail.com>
2021-04-17 19:02:47 +0800

    86

--- src/86/index.js
+++ src/86/index.js
@@ -1,4 +1,5 @@
 import { group } from "../utils.js";
+import { arrayToListNode, listNodeToArray } from "../23/index.js";
 
 /**
  * Definition for singly-linked list.
@@ -14,11 +15,35 @@ import { group } from "../utils.js";
  * @return {ListNode}
  */
 const partition = function (head, x) {
-  return head;
+  const q = [];
+  while (head && head.val < x) {
+    q.push(head);
+    head = head.next;
+  }
+
+  for (let t = head; t && t.next; ) {
+    if (t.next.val < x) {
+      q.push(t.next);
+      t.next = t.next.next;
+    } else {
+      t = t.next;
+    }
+  }
+
+  for (let i = 0; i < q.length - 1; i++) {
+    q[i].next = q[i + 1];
+  }
+
+  if (q.length === 0) {
+    return head;
+  }
+  q[q.length - 1].next = head;
+  return q[0];
 };
 
 export default function run(input) {
   return group(input.map(JSON.parse), 2)
-    .map(([head, x]) => partition(head, x))
+    .map(([head, x]) => partition(arrayToListNode(head), x))
+    .map(listNodeToArray)
     .map((v) => JSON.stringify(v));
 }
