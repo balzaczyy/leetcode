97acf0225414a33f8d5c143b0a65a2dfa74a7e45
zhouyiyan <balzaczyy@gmail.com>
2021-04-17 21:58:12 +0800

    341 WA

--- src/341/index.js
+++ src/341/index.js
@@ -6,23 +6,39 @@ import { group } from "../utils.js";
  */
 function NestedInteger(value) {
   this.value = value;
-  this.isInteger = () => value instanceof Number;
-  this.getInteger = () => (value instanceof Number ? value : null);
-  this.getList = () => (value instanceof Number ? null : value);
+  this.isInteger = () => typeof value === "number";
+  /**
+   * @returns {Number|null}
+   */
+  this.getInteger = () => (this.isInteger() ? value : null);
+  /**
+   * @returns {NestedInteger[]|null}
+   */
+  this.getList = () => (this.isInteger() ? null : value);
 }
 
 /**
  * @constructor
  * @param {NestedInteger[]} nestedList
  */
-var NestedIterator = function (nestedList) {};
+const NestedIterator = function (nestedList) {
+  this.nestedList = nestedList;
+  // find very first value
+  const stack = [];
+  let candidates = nestedList;
+  do {
+    stack.push(0);
+    candidates = candidates[0].getList();
+  } while (candidates);
+  this.stack = stack;
+};
 
 /**
  * @this NestedIterator
  * @returns {boolean}
  */
 NestedIterator.prototype.hasNext = function () {
-  return false;
+  return this.stack.length > 0 && this.stack[0] < this.nestedList.length;
 };
 
 /**
@@ -30,7 +46,31 @@ NestedIterator.prototype.hasNext = function () {
  * @returns {number}
  */
 NestedIterator.prototype.next = function () {
-  return 0;
+  const q = [[...this.nestedList]];
+  /**
+   * @type {NestedInteger}
+   */
+  let t;
+  for (let i = 0; i < this.stack.length; i++) {
+    t = q[q.length - 1][this.stack[i]];
+    const next = t.getList();
+    if (next) {
+      q.push(next);
+    }
+  }
+  // move cursor
+  this.stack[this.stack.length - 1]++;
+  while (
+    this.stack.length > 0 &&
+    this.stack[this.stack.length - 1] === q[this.stack.length - 1].length
+  ) {
+    this.stack.pop();
+    q.pop();
+    if (this.stack.length > 0) {
+      this.stack[this.stack.length - 1]++;
+    }
+  }
+  return t.getInteger();
 };
 
 /**
@@ -49,7 +89,7 @@ function arrayToNestedInteger(arr) {
    */
   const q = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] instanceof Number) {
+    if (typeof arr[i] === "number") {
       q.push(new NestedInteger(arr[i]));
     } else if (Array.isArray(arr[i])) {
       q.push(new NestedInteger(arrayToNestedInteger(arr[i])));
