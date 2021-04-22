dcd8743cec201e3996b3bd4f0017926869ea687a
zhouyiyan <balzaczyy@gmail.com>
2021-04-22 18:07:51 +0800

    554 TLE

--- src/554/index.js
+++ src/554/index.js
@@ -3,7 +3,26 @@
  * @return {number}
  */
 const leastBricks = function (wall) {
-  return 0;
+  const height = wall.length;
+  const plan = Array(height);
+  for (let i = 0; i < height; i++) {
+    plan[i] = [i, wall[i].shift()];
+  }
+  let ans = 0;
+  let count = wall.reduce((acc, cur) => acc + cur.length, 0);
+  while (count > 0) {
+    plan.sort((a, b) => a[1] - b[1]);
+    const score = plan.findIndex((v) => v[1] !== plan[0][1]);
+    if (score > ans) {
+      ans = score;
+      // console.log(score, plan);
+    }
+    for (let i = 0; i < score; i++) {
+      plan[i][1] += wall[plan[i][0]].shift();
+      count--;
+    }
+  }
+  return height - ans;
 };
 
 export default function run(input) {
