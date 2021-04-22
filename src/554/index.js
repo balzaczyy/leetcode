c8f2d52ca94b992180520d9e91c539c145291643
zhouyiyan <balzaczyy@gmail.com>
2021-04-22 17:51:29 +0800

    554

---
+++ src/554/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[][]} wall
+ * @return {number}
+ */
+const leastBricks = function (wall) {
+  return 0;
+};
+
+export default function run(input) {
+  return input.map(JSON.parse).map(leastBricks).map(String);
+}
