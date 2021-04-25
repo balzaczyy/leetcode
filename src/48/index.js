c17e79fe3495842370221da070ad465b405256c4
zhouyiyan <balzaczyy@gmail.com>
2021-04-25 17:47:23 +0800

    48

--- src/48/index.js
+++ src/48/index.js
@@ -2,7 +2,28 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-const rotate = function (matrix) {};
+const rotate = function (matrix) {
+  const d = matrix.length;
+  (function xy(matrix) {
+    for (let i = 0; i < d; i++) {
+      for (let j = i + 1; j < d; j++) {
+        const t = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = t;
+      }
+    }
+  })(matrix);
+  // console.log(matrix);
+  (function mirrorX(matrix) {
+    for (let i = 0; i < d; i++) {
+      for (let j = 0; j < d / 2; j++) {
+        const t = matrix[i][j];
+        matrix[i][j] = matrix[i][d - 1 - j];
+        matrix[i][d - 1 - j] = t;
+      }
+    }
+  })(matrix);
+};
 
 export default function run(input) {
   return input
