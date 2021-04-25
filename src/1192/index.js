9d7ab039c0dedef897e4eb9e5ba5dfe8912f4567
zhouyiyan <balzaczyy@gmail.com>
2021-04-25 21:22:07 +0800

    1192 WA

--- src/1192/index.js
+++ src/1192/index.js
@@ -6,7 +6,51 @@ import { group } from "../utils.js";
  * @return {number[][]}
  */
 const criticalConnections = function (n, connections) {
-  return [];
+  const edges = new Map();
+  for (let i = 0; i < n; i++) {
+    edges.set(i, []);
+  }
+  for (const conn of connections) {
+    const [from, to] = conn;
+    edges.get(from).push(to);
+    edges.get(to).push(from);
+  }
+
+  const ans = [];
+  const q = [];
+  const visited = new Map();
+  q.push({ id: 0 });
+  visited.set(0, 0);
+  while (q.length > 0) {
+    const cur = q[q.length - 1];
+    const { id, next } = cur;
+    const nextId = cur.next !== undefined ? next + 1 : 0;
+    cur.next = nextId;
+    const candidates = edges.get(id);
+    if (nextId < candidates.length) {
+      const target = candidates[nextId];
+      if (visited.has(target)) {
+        if (visited.get(cur.id) - visited.get(target) > 1) {
+          // loop detected
+          cur.back = target;
+        } // else ignore
+      } else {
+        q.push({ id: target });
+        visited.set(target, q.length - 1);
+      }
+    } else {
+      q.pop();
+      if (q.length > 0) {
+        if (cur.back === undefined) {
+          ans.push([q[q.length - 1].id, cur.id]);
+        } else if (cur.back !== id) {
+          q[q.length - 1].back = cur.back;
+        }
+      }
+    }
+  }
+
+  return ans;
 };
 
 export default function run(input) {
