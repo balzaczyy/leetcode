dc164ef0dc2b6cdf38dbde43ef7c156e179e3800
zhouyiyan <balzaczyy@gmail.com>
2021-04-25 18:06:11 +0800

    696

--- src/696/index.js
+++ src/696/index.js
@@ -3,7 +3,19 @@
  * @return {number}
  */
 const countBinarySubstrings = function (s) {
-  return 0;
+  let pos = 0,
+    last = 0,
+    ans = 0;
+  while (pos < s.length) {
+    let count = 1;
+    while (pos + count < s.length && s[pos + count] === s[pos]) {
+      count++;
+    }
+    ans += Math.min(last, count);
+    last = count;
+    pos += count;
+  }
+  return ans;
 };
 
 export default function run(input) {
