6f5f0e03bccf9c7927be30eb488f9e8efd8f74c1
zhouyiyan <balzaczyy@gmail.com>
2021-04-30 16:47:37 +0800

    34

--- src/34/index.js
+++ src/34/index.js
@@ -6,7 +6,22 @@ import { group } from "../utils.js";
  * @return {number[]}
  */
 const searchRange = function (nums, target) {
-  return [-1, -1];
+  let start = 0;
+  let last = [null, 0];
+  for (let i = 0; i < nums.length; i++) {
+    const d = nums[i];
+    if (d > target) {
+      // done
+      break;
+    }
+    if (d !== last[0]) {
+      start += last[1];
+      last = [d, 1];
+    } else {
+      last[1]++;
+    }
+  }
+  return last[0] === target ? [start, start + last[1] - 1] : [-1, -1];
 };
 
 export default function run(input) {
