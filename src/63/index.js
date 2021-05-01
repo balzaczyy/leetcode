c7dde78f5a5174f81704c20e5b9db3d45e5a7fd9
zhouyiyan <balzaczyy@gmail.com>
2021-05-01 21:00:59 +0800

    63

--- src/63/index.js
+++ src/63/index.js
@@ -3,7 +3,31 @@
  * @return {number}
  */
 const uniquePathsWithObstacles = function (obstacleGrid) {
-  return 1;
+  const rowLen = obstacleGrid.length;
+  const colLen = obstacleGrid[0].length;
+  const score = (y, x) => {
+    if (y < 0 || x < 0) {
+      return 0;
+    }
+    return obstacleGrid[y][x];
+  };
+  for (let i = 0; i < rowLen; i++) {
+    for (let j = 0; j < colLen; j++) {
+      if (i === 0 && j === 0) {
+        if (obstacleGrid[i][j] === 1) {
+          return 0;
+        }
+        obstacleGrid[i][j] = 1; // start
+      } else if (obstacleGrid[i][j] === 1) {
+        // stone
+        obstacleGrid[i][j] = 0;
+      } else {
+        obstacleGrid[i][j] = score(i - 1, j) + score(i, j - 1);
+      }
+    }
+  }
+  // console.log(obstacleGrid)
+  return obstacleGrid[rowLen - 1][colLen - 1];
 };
 
 export default function run(input) {
