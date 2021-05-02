59af6874eff4479a18ec1137e875812acb66a967
zhouyiyan <balzaczyy@gmail.com>
2021-05-02 11:37:03 +0800

    326

--- src/326/index.js
+++ src/326/index.js
@@ -3,7 +3,13 @@
  * @return {boolean}
  */
 const isPowerOfThree = function (n) {
-  return false;
+  if (n < 1) {
+    return false;
+  }
+  const minDiff = 0.0000000001;
+  const ans = Math.log(n) / Math.log(3);
+  // console.log(ans);
+  return Math.abs(ans - Math.floor(ans)) < minDiff;
 };
 
 export default function run(input) {
