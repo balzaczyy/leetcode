d730ee953d4686e6a1f1f1d948df47be8f196b5d
zhouyiyan <balzaczyy@gmail.com>
2021-05-02 13:02:06 +0800

    1642 WA

--- src/1642/index.js
+++ src/1642/index.js
@@ -7,7 +7,27 @@ import { group } from "../utils.js";
  * @return {number}
  */
 const furthestBuilding = function (heights, bricks, ladders) {
-  return 0;
+  let cuts = [],
+    usedBricks = 0;
+  let ups = 0;
+  for (let i = 1; i < heights.length; i++) {
+    const diff = heights[i] - heights[i - 1];
+    if (diff > 0) {
+      ups++;
+      cuts.push(diff);
+      if (usedBricks + diff <= bricks) {
+        usedBricks += diff;
+      } else {
+        cuts.sort((a, b) => a - b);
+        usedBricks += cuts[cuts.length - 1] - diff;
+        cuts.pop();
+      }
+      if (ups - cuts.length > ladders) {
+        return i - 1;
+      }
+    }
+  }
+  return heights.length - 1;
 };
 
 export default function run(input) {
