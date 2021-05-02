d0f966db9112416f232ff91aa0157bb3895fd220
zhouyiyan <balzaczyy@gmail.com>
2021-05-02 15:37:19 +0800

    1642 WA

--- src/1642/index.js
+++ src/1642/index.js
@@ -7,6 +7,27 @@ import { group } from "../utils.js";
  * @return {number}
  */
 const furthestBuilding = function (heights, bricks, ladders) {
+  function bicert(arr, value, start = 0, end = arr.length) {
+    while (start < end) {
+      if (value < arr[start]) {
+        arr.splice(start, 0, value);
+        return;
+      } else if (value > arr[end - 1]) {
+        arr.splice(end, 0, value);
+        return;
+      } else {
+        const mid = Math.floor((start + end) / 2);
+        if (arr[mid] === value) {
+          arr.splice(mid, 0, value);
+          return;
+        } else if (arr[mid] < value) {
+          start = mid + 1;
+        } else {
+          end = mid - 1;
+        }
+      }
+    }
+  }
   let cuts = [],
     usedBricks = 0;
   let ups = 0;
@@ -14,12 +35,24 @@ const furthestBuilding = function (heights, bricks, ladders) {
     const diff = heights[i] - heights[i - 1];
     if (diff > 0) {
       ups++;
-      cuts.push(diff);
+      if (cuts.length === 0 || diff > cuts[cuts.length - 1]) {
+        cuts.push(diff);
+      } else if (diff < cuts[0]) {
+        cuts.unshift(diff);
+      } else {
+        bicert(cuts, diff);
+        // const pos = cuts.findIndex(v => v >= diff);
+        // if (pos < 0) {
+        //   cuts.push(diff);
+        // } else {
+        //   cuts.splice(pos, 0, diff);
+        // }
+      }
       if (usedBricks + diff <= bricks) {
         usedBricks += diff;
       } else {
-        cuts.sort((a, b) => a - b);
-        usedBricks += cuts[cuts.length - 1] - diff;
+        // cuts.sort((a, b) => a - b);
+        usedBricks += diff - cuts[cuts.length - 1];
         cuts.pop();
       }
       if (ups - cuts.length > ladders) {
