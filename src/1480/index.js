97e6ed6823daafffcb2206aa32768d6050b363da
zhouyiyan <balzaczyy@gmail.com>
2021-05-03 19:43:23 +0800

    1480

--- src/1480/index.js
+++ src/1480/index.js
@@ -3,7 +3,12 @@
  * @return {number[]}
  */
 const runningSum = function (nums) {
-  return nums;
+  const ans = Array(nums.length);
+  ans[0] = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    ans[i] = ans[i - 1] + nums[i];
+  }
+  return ans;
 };
 
 export default function run(input) {
