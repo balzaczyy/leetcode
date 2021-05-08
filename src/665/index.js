5bcd049c06ff75d773c831ad1850b7ac8c6cc9b7
zhouyiyan <balzaczyy@gmail.com>
2021-05-08 11:14:46 +0800

    665 WA

--- src/665/index.js
+++ src/665/index.js
@@ -3,6 +3,25 @@
  * @return {boolean}
  */
 const checkPossibility = function (nums) {
+  if (nums.length === 1) {
+    return true;
+  }
+  let count = 0,
+    water = Math.min(nums[0], nums[1]);
+  for (let i = 0; i < nums.length - 1; i++) {
+    const diff = nums[i] - nums[i + 1];
+    if (diff > 0) {
+      if (nums[i + 1] < water) {
+        return false;
+      }
+      count++;
+      if (count > 1) {
+        return false;
+      }
+    } else {
+      water = nums[i];
+    }
+  }
   return true;
 };
 
