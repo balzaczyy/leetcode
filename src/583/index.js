6a2426fa91a5d237f3b5fb65304fc07cd00c2cce
zhouyiyan <balzaczyy@gmail.com>
2021-05-08 08:42:34 +0800

    1480 LCS

--- src/583/index.js
+++ src/583/index.js
@@ -6,62 +6,21 @@ import { group } from "../utils.js";
  * @return {number}
  */
 const minDistance = function (word1, word2) {
-  if (word1.localeCompare(word2) === 0) {
-    return 0;
-  }
-  const b1 = new Map(),
-    b2 = new Map();
-  b1.set(word1, 0);
-  b1.set("", word1.length);
-  b2.set(word2, 0);
-  b2.set("", word2.length);
-  const q1 = [word1],
-    q2 = [word2];
-
-  /**
-   * @param {[]} q
-   * @param {number} target
-   * @param {Map} bucket1
-   * @param {Map} bucket2
-   * @returns {[boolean, number]}
-   */
-  function extend(q, target, bucket1, bucket2) {
-    while (q[0].length > target) {
-      const w = q.shift();
-      const score = bucket1.get(w);
-      for (let i = 0; i < w.length; i++) {
-        const ws = w
-          .split("")
-          .filter((v, index) => index !== i)
-          .join("");
-        if (bucket2.has(ws)) {
-          return [true, score + 1 + bucket2.get(ws)];
-        }
-        if (!bucket1.has(ws)) {
-          bucket1.set(ws, score + 1);
-          q.push(ws);
-        }
+  const ans = Array(word1.length + 1);
+  for (let i = 0; i <= word1.length; i++) {
+    ans[i] = Array(word2.length + 1);
+    for (let j = 0; j <= word2.length; j++) {
+      if (i === 0 || j === 0) {
+        ans[i][j] = 0;
+      } else if (word1[i - 1] === word2[j - 1]) {
+        ans[i][j] = 1 + ans[i - 1][j - 1];
+      } else {
+        ans[i][j] = Math.max(ans[i - 1][j], ans[i][j - 1]);
       }
     }
-    return [false];
   }
-
-  let found = false,
-    res;
-  for (
-    let target = Math.min(word1.length, word2.length);
-    !found && target > 0;
-    target--
-  ) {
-    if (q1.length === 0 || q2.length === 0) {
-      throw new Error("impossible");
-    }
-    [found, res] = extend(q1, target, b1, b2);
-    if (!found) {
-      [found, res] = extend(q2, target, b2, b1);
-    }
-  }
-  return res || word1.length + word2.length;
+  // console.log(ans);
+  return word1.length + word2.length - ans[word1.length][word2.length] * 2;
 };
 
 export default function run(input) {
