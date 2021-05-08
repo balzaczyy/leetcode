70eeab0642e7b706d549e1ade3147bd1b16bdfb6
zhouyiyan <balzaczyy@gmail.com>
2021-05-08 09:21:29 +0800

    45

--- src/45/index.js
+++ src/45/index.js
@@ -3,7 +3,18 @@
  * @return {number}
  */
 const jump = function (nums) {
-  return 0;
+  const len = nums.length;
+  const ans = Array(len);
+  ans[0] = 0;
+  for (let i = 1; i < len; i++) {
+    ans[i] = ans[i - 1] + 1;
+    for (let j = 0; j < i; j++) {
+      if (j + nums[j] >= i) {
+        ans[i] = Math.min(ans[i], ans[j] + 1);
+      }
+    }
+  }
+  return ans[len - 1];
 };
 
 export default function run(input) {
