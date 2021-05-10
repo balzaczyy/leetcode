26bd6600ab1d3b52a1cdf628281d7a4d89ceb50c
zhouyiyan <balzaczyy@gmail.com>
2021-05-10 20:51:37 +0800

    1354 WA

--- src/1354/index.js
+++ src/1354/index.js
@@ -3,7 +3,23 @@
  * @return {boolean}
  */
 const isPossible = function (target) {
-  return false;
+  if (target.length === 1) {
+    return target[0] === 1;
+  }
+
+  target.sort((a, b) => b - a);
+  const n = target.length;
+  while (target[0] > 1) {
+    for (let i = 0; i < n; i++) {
+      for (let j = 1; j < n; j++) {
+        target[i] -= target[(i + j) % n];
+        if (target[i] < 1) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
 };
 
 export default function run(input) {
