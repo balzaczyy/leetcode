4079986683828a86f13e1e4af3380aac4095d05f
zhouyiyan <balzaczyy@gmail.com>
2021-06-07 17:44:41 +0800

    746

--- src/746/index.js
+++ src/746/index.js
@@ -3,7 +3,14 @@
  * @return {number}
  */
 const minCostClimbingStairs = function (cost) {
-  return 0;
+  const len = cost.length;
+  const ans = Array(len + 2);
+  ans[len] = 0;
+  ans[len + 1] = 0;
+  for (let i = len - 1; i >= 0; i--) {
+    ans[i] = cost[i] + Math.min(ans[i + 1], ans[i + 2]);
+  }
+  return Math.min(ans[0], ans[1]);
 };
 
 export default function run(input) {
